74946a33b55f36119ae887cb3e2e4e102c4416b6
VSAmrutha <Amrutha.V.S@ibm.com>
2021-06-22 12:51:24 +0530

    counter path

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./App.css";
-import Counter from "./Counter/Counter";
+import Counter from "./components/Counter/Counter";
 import Posts from "./components/Posts/Posts";
 // import Todo from "./components/Todo/Todo";
 function App() {
@@ -18,12 +18,18 @@ function App() {
         return <Todo todo={todo} />;
       })} */}
       <div className="tabs">
-      <button className="generic-btn button-primary" onClick={() => displayHandler("1")}>
-        Posts
-      </button>
-      <button className="generic-btn button-secondary" onClick={() => displayHandler("2")}>
-        Counter
-      </button>
+        <button
+          className="generic-btn button-primary"
+          onClick={() => displayHandler("1")}
+        >
+          Posts
+        </button>
+        <button
+          className="generic-btn button-secondary"
+          onClick={() => displayHandler("2")}
+        >
+          Counter
+        </button>
       </div>
       {display === "1" && <Posts />}
       {display === "2" && <Counter />}
